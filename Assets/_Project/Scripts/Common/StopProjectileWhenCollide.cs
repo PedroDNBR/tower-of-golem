d808ac25e462050e221ba4d14952b7cba039cc21
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2025-09-15 10:46:12 -0300

    fix: boss hitting himself with sword

--- Assets/_Project/Scripts/Common/StopProjectileWhenCollide.cs
+++ Assets/_Project/Scripts/Common/StopProjectileWhenCollide.cs
@@ -5,19 +5,36 @@ namespace TW
     public class StopProjectileWhenCollide : MonoBehaviour
     {
         Projectile projectile;
+        public Transform origin;
+        public LayerMask ignoreLayer;
 
         void OnEnable()
         {
             projectile = GetComponent<Projectile>();
+            DealDamageWhenTriggerEnter damageWhenTriggerEnter = GetComponent<DealDamageWhenTriggerEnter>();
+            if (damageWhenTriggerEnter != null)
+                if (damageWhenTriggerEnter.CharacterBaseHealth != null)
+                    origin = damageWhenTriggerEnter.CharacterBaseHealth.transform.root;
         }
 
         private void OnTriggerEnter(Collider other)
         {
+            if(origin == null)
+            {
+                DealDamageWhenTriggerEnter damageWhenTriggerEnter = GetComponent<DealDamageWhenTriggerEnter>();
+                if (damageWhenTriggerEnter != null)
+                    origin = damageWhenTriggerEnter.CharacterBaseHealth.transform.root;
+            }
+
+            if (other.transform.root == origin) return;
+            if (((1 << other.gameObject.layer) & ignoreLayer.value) == 0) return;
             if (projectile == null) return;
             Invoke(nameof(StopProjectile), 0.01f);
 
             transform.parent = other.transform;
-            
+            Rigidbody rigid = GetComponent<Rigidbody>();
+            if (rigid != null)
+                Destroy(rigid);
         }
 
         private void StopProjectile()
